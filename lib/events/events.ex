3b56c4a4db706c3aace9386e0cbc0fa935f0cda6
mihai-safta <mihai.safta@hootsuite.com>
2017-10-07 17:59:15 +0300

    Fix json marshaling errors for msg (converting to plain string) Fix pid formatting for :error_logger with format_pid

--- lib/events/events.ex
+++ lib/events/events.ex
@@ -5,19 +5,19 @@ defmodule Events do
 
   @spec actor_started(String.t, identifier()) :: %{ev_type: term(), module: Strong.t, pid: identifier(), ts: ts}
   def actor_started(module, pid) do
-    %{:ev_type => :actor_started, :module => module, :pid => to_string(:erlang.pid_to_list(pid)), :ts => timestamp_to_millis(:erlang.timestamp)}
+    %{:ev_type => :actor_started, :module => module, :pid => format_pid(pid), :ts => timestamp_to_millis(:erlang.timestamp)}
   end
 
   def send_msg_event(pid, to_pid, msg, ts) do
-    %{:ev_type => :message_sent, :pid => to_string(:erlang.pid_to_list(pid)), :to_pid => to_string(:erlang.pid_to_list(to_pid)), :msg => msg, :ts => timestamp_to_millis(ts)}
+    %{:ev_type => :message_sent, :pid => to_string(:erlang.pid_to_list(pid)), :to_pid => format_pid(to_pid), :msg => inspect(msg), :ts => timestamp_to_millis(ts)}
   end
 
   def receive_msg_event(pid, msg, ts) do
-    %{:ev_type => :message_received, :pid => to_string(:erlang.pid_to_list(pid)), :msg => msg, :ts => timestamp_to_millis(ts)}
+    %{:ev_type => :message_received, :pid => format_pid(pid), :msg => inspect(msg), :ts => timestamp_to_millis(ts)}
   end
 
   def actor_stoped(pid, ts) do
-    %{:ev_type => :actor_stopped, :pid => to_string(:erlang.pid_to_list(pid)), :ts => timestamp_to_millis(ts)}
+    %{:ev_type => :actor_stopped, :pid => format_pid(pid), :ts => timestamp_to_millis(ts)}
   end
 
   defp timestamp_to_millis(ts) do
@@ -25,4 +25,10 @@ defmodule Events do
     (mega*1000000 + seconds)*1000 + :erlang.round(ms/1000)
   end
 
+  defp format_pid(pid) do
+    case pid do
+      :error_logger -> :error_logger
+      p when is_pid(p) -> to_string(:erlang.pid_to_list(p))
+    end
+  end
 end
