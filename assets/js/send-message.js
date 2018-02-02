27c2968af16ff2bf56560501cc2e04f3440d638d
Mihai Safta <saftacatalinmihai@gmail.com>
2018-02-02 08:29:18 +0200

    Added Relay actor type. Relays a message to another actor.

--- assets/js/send-message.js
+++ assets/js/send-message.js
@@ -2,27 +2,27 @@ import channel from "./socket"
 import $ from "jquery"
 import {success, error} from "./notifications";
 
-let TO_PID = undefined
+let TO_PID = undefined;
 
 export function send_message(toPid) {
-    $('#msg-modal').modal('open')
-    $("#msg").focus()
+    $('#msg-modal').modal('open');
+    $("#msg").focus();
     TO_PID = toPid
 }
 
 $("#msg-from").submit(event => {
-    $('#msg-modal').modal('close')
-    $("svg").focus()
-    event.preventDefault()
-    let msg = $("#msg").val()
-    channel.push("send_msg", {"to": TO_PID, "msg": "\"" + msg + "\""})
+    $('#msg-modal').modal('close');
+    $("svg").focus();
+    event.preventDefault();
+    let msg = $("#msg").val();
+    channel.push("send_msg", {"to": TO_PID, "msg": msg})
         .receive("ok", resp => {
-            success(resp['received'])
+            success(resp['received']);
             console.log("Resp:", resp)
         })
         .receive("error", resp => {
-            let message = "Unable to send message: [" + msg + "] to actor: [" + TO_PID + "]"
-            error(message)
+            let message = "Unable to send message: [" + msg + "] to actor: [" + TO_PID + "]";
+            error(message);
             console.log(console.log(message + ". Error: ", resp))
         })
-})
+});
---
+++ code/Relay.ex
@@ -0,0 +1,58 @@
+defmodule Relay do
+  use GenServer
+  require Logger
+  require ActorTracer
+
+  def start do
+    {:ok, pid} = GenServer.start(__MODULE__, :ok, [])
+    ActorTracer.trace(pid)
+    {:ok, pid}
+  end
+
+  def start_link do
+    {:ok, pid} = GenServer.start_link(__MODULE__, :ok, [])
+    ActorTracer.trace(pid)
+    {:ok, pid}
+  end
+
+  def ping(server) do
+    GenServer.call(server, "ping")
+  end
+
+  def send_msg(server, msg) do
+    GenServer.call(server, msg)
+  end
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call("ping", _from, state) do
+    {:reply, "pong", state}
+  end
+  
+  def handle_call({"relay", pid, msg}, _from, state) do
+    pid = :erlang.list_to_pid(to_charlist("<0.#{pid}.0>"))
+  	{:reply, GenServer.call(pid, msg), state}
+  end
+
+  def handle_call(_, _from, state) do
+    {:reply, "?", state}
+  end
+
+  def handle_cast("ping", state) do
+    {:noreply, state}
+  end
+
+  def handle_cast(_, state) do
+    {:noreply, state}
+  end
+
+  def code_change(old_vsn, old_state, _extra) do
+    IO.puts "Code change"
+    Logger.info "Code change"
+    IO.inspect old_vsn
+    {:ok, old_state}
+  end
+
+end
